a1cae361c1b68a72d4b0c14f9a8e5879c4128188
kenta-afk <kentarou9.60@icloud.com>
2025-07-10 16:49:58 +0900

    add: /play/create endpoint

--- models/play.py
+++ models/play.py
@@ -2,6 +2,19 @@ from pydantic import BaseModel
 from typing import Optional
 
 class Scenarioes(BaseModel):
-    request1: Optional[str]
-    request2: Optional[str]
-    senarioe: Optional[str]
+    request1: Optional[str] = None
+    request2: Optional[str] = None
+    senarioe: Optional[str] = None
+
+class CreateGameRequest(BaseModel):
+    scenarioes: str
+
+class CreateGameResponse(BaseModel):
+    user_id: str
+    game_id: str
+    struct: Optional[dict] = None
+    funds: int
+    current_month: int
+    scenarioes: str
+    is_finished: bool
+    created_at: str
