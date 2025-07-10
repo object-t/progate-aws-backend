b95fd833b9cb5e290e2f3c15741c8f8216172fef
kenta-afk <kentarou9.60@icloud.com>
2025-07-10 11:39:35 +0900

    add: get scenarioes endpoint

--- routers/play.py
+++ routers/play.py
@@ -1,29 +1,35 @@
 from fastapi import APIRouter, HTTPException, Depends
 import models.play as play_models
 import boto3
+from boto3.dynamodb.conditions import Key
 from settings import get_DynamoDbConnect
 from routers.extractor import extract_user_id_from_token
 
 play_router = APIRouter()
 
-# settings = get_DynamoDbConnect()
+settings = get_DynamoDbConnect()
 
-# DYNAMODB_ENDPOINT = settings.DYNAMODB_ENDPOINT
-# REGION = settings.REGION
-# AWS_ACCESS_KEY_ID = settings.AWS_ACCESS_KEY_ID
-# AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
+DYNAMODB_ENDPOINT = settings.DYNAMODB_ENDPOINT
+REGION = settings.REGION
+AWS_ACCESS_KEY_ID = settings.AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
 
-# dynamodb = boto3.resource(
-#     "dynamodb",
-#     endpoint_url=DYNAMODB_ENDPOINT,
-#     region_name=REGION,
-#     aws_access_key_id=AWS_ACCESS_KEY_ID,
-#     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-# )
+dynamodb = boto3.resource(
+    "dynamodb",
+    endpoint_url=DYNAMODB_ENDPOINT,
+    region_name=REGION,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
 
-# table_name = "game"
-# table = dynamodb.Table(table_name)
+table_name = "game"
+table = dynamodb.Table(table_name)
 
 @play_router.get("/play/scenarioes")
-async def get_scenarioes(UserId: str = Depends(extract_user_id_from_token)):
-    return {"return senarioes"}
+async def get_scenarioes(user_id: str = Depends(extract_user_id_from_token)) -> play_models.Scenarioes:
+    response = table.query(
+        KeyConditionExpression=Key("PK").eq("entity") & Key("SK").eq("metadata")
+    )
+    data = response.get("Items", [{}])[0].get("scenarioes", {})
+
+    return play_models.Scenarioes(**data)
